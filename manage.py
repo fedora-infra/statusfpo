13b5eceff09365cd2c8ac0b32247ed32e3a43cbc
Patrick Uiterwijk <puiterwijk@gmail.com>
2012-08-07 01:59:02 +0200

    Finish move of manage.py

--- manage.py
+++ manage.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     f.close()
 
     if not args.no_git:
-        result = subprocess.call("git add statuses.json", shell=True)
+        result = subprocess.call("git add wsgi/statuses.json", shell=True)
     if result != 0:
         print('An error occured in git add, please try again!')
         sys.exit(4)
