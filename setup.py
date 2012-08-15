5112477e2aaceaa3a20c9da7d116a24520acdb1b
Patrick Uiterwijk <puiterwijk@gmail.com>
2012-08-16 00:15:25 +0200

    Require argparse

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='FedoraStatus',
       author='Patrick Uiterwijk',
       author_email='puiterwijk@fedoraproject.org',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['jinja2'],
+      install_requires=['jinja2', 'argparse'],
 #      install_requires=['Django>=1.3'],
      )
