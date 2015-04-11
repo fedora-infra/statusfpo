8d695fd31a63d4e9d441295a41f10a2e3446ce61
Kevin Fenzi <kevin@scrye.com>
2015-04-11 12:21:12 -0600

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/changes.js
+++ wsgi/changes.js
@@ -1,5 +1,13 @@
 callback([
     {
+        "status": "good", 
+        "new_global_status": "good", 
+        "service": "koji", 
+        "changetype": "single", 
+        "message": "Everything seems to be working.", 
+        "changedate": 1428776472.194095
+    }, 
+    {
         "status": "scheduled", 
         "new_global_status": "scheduled", 
         "service": "koji", 
