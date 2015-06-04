bf40632f916fbfc0419ab5c41cb4322be654e63b
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-06-04 14:45:47 +0200

    New service status for ['copr']: minor, with message: COPR frontend is back, builds will get back soon

--- wsgi/changes.js
+++ wsgi/changes.js
@@ -1,5 +1,13 @@
 callback([
     {
+        "status": "minor", 
+        "new_global_status": "minor", 
+        "service": "copr", 
+        "changetype": "single", 
+        "message": "COPR frontend is back, builds will get back soon", 
+        "changedate": 1433421947.499779
+    }, 
+    {
         "status": "major", 
         "new_global_status": "major", 
         "service": "copr", 
