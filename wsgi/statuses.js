5f5f81c6a3458ef94421ed172f883b91f81ab96a
Kevin Fenzi <kevin@scrye.com>
2015-06-19 11:13:52 -0600

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -1,169 +1,169 @@
 callback({
-    "global_info": "network issues being worked on", 
+    "global_info": "", 
     "global_status": "minor", 
     "global_verbose_status": "Minor service disruption", 
     "services": {
         "ask": {
             "message": "Everything seems to be working.", 
             "name": "Ask Fedora", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://ask.fedoraproject.org/"
         }, 
         "authentication": {
             "message": "Everything seems to be working.", 
             "name": "FedOAuth", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://id.fedoraproject.org/"
         }, 
         "badges": {
             "message": "Everything seems to be working.", 
             "name": "Badges", 
-            "status": "major", 
+            "status": "good", 
             "url": "http:/badges.fedoraproject.org/"
         }, 
         "blockerbugs": {
             "message": "Everything seems to be working.", 
             "name": "Blockerbugs", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://qa.fedoraproject.org/blockerbugs/"
         }, 
         "bodhi": {
             "message": "Everything seems to be working.", 
             "name": "Package Updates Manager", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://admin.fedoraproject.org/updates/"
         }, 
         "copr": {
             "message": "Everything seems to be working.", 
             "name": "COPR Build System", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://copr.fedoraproject.org/"
         }, 
         "darkserver": {
             "message": "Everything seems to be working.", 
             "name": "Darkserver", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://fedoraproject.org/wiki/Darkserver"
         }, 
         "docs": {
             "message": "Everything seems to be working.", 
             "name": "Documentation website", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://docs.fedoraproject.org/"
         }, 
         "elections": {
             "message": "Everything seems to be working.", 
             "name": "Fedora elections", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://admin.fedoraproject.org/voting/"
         }, 
         "fas": {
             "message": "Everything seems to be working.", 
             "name": "Account System", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://admin.fedoraproject.org/accounts/"
         }, 
         "fedmsg": {
             "message": "Everything seems to be working.", 
             "name": "Fedora Messaging Bus", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://admin.fedoraproject.org/pkgdb/acls/name/fedmsg"
         }, 
         "fedocal": {
             "message": "Everything seems to be working.", 
             "name": "Fedora Calendar", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://apps.fedoraproject.org/fedocal/"
         }, 
         "fedorahosted": {
             "message": "Everything seems to be working.", 
             "name": "FedoraHosted.org Services", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://fedorahosted.org/web/"
         }, 
         "fedorapaste": {
             "message": "Everything seems to be working.", 
             "name": "Fedora pastebin service", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://paste.fedorahosted.org/"
         }, 
         "freemedia": {
             "message": "Everything seems to be working.", 
             "name": "FreeMedia", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://fedoraproject.org/wiki/FreeMedia"
         }, 
         "koji": {
             "message": "Everything seems to be working.", 
             "name": "The Koji Buildsystem", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://koji.fedoraproject.org/koji/"
         }, 
         "mailinglists": {
             "message": "Everything seems to be working.", 
             "name": "Mailing Lists", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://lists.fedoraproject.org/"
         }, 
         "mirrorlist": {
             "message": "Everything seems to be working.", 
             "name": "Mirror List", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://mirrors.fedoraproject.org/publiclist/"
         }, 
         "mirrormanager": {
             "message": "Everything seems to be working.", 
             "name": "Mirror Manager", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://mirrors.fedoraproject.org/"
         }, 
         "packages": {
             "message": "Everything seems to be working.", 
             "name": "Fedora Packages App", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://apps.fedoraproject.org/packages"
         }, 
         "people": {
             "message": "Everything seems to be working.", 
             "name": "Fedora People", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://fedorapeople.org/"
         }, 
         "pkgdb": {
             "message": "Everything seems to be working.", 
             "name": "Package Database", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://admin.fedoraproject.org/pkgdb/"
         }, 
         "pkgs": {
             "mailinglists": "Fedora mailing lists", 
             "message": "Everything seems to be working.", 
             "name": "Package maintainers git repositories", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://pkgs.fedoraproject.org/"
         }, 
         "tagger": {
             "message": "Everything seems to be working.", 
             "name": "Tagger", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://apps.fedoraproject.org/tagger/"
         }, 
         "website": {
             "message": "Everything seems to be working.", 
             "name": "Fedora websites", 
-            "status": "major", 
+            "status": "good", 
             "url": "http://fedoraproject.org/"
         }, 
         "wiki": {
             "message": "Everything seems to be working.", 
             "name": "Fedora Wiki", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://fedoraproject.org/wiki/Fedora_Project_Wiki"
         }, 
         "zodbot": {
             "message": "Everything seems to be working.", 
             "name": "Zodbot IRC bot", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://fedoraproject.org/wiki/Zodbot"
         }
     }
