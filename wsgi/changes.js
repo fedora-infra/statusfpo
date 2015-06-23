b21ee50c98e1b2231c69894e3bf845df8ab3f67e
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-06-23 12:36:51 +0200

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/changes.js
+++ wsgi/changes.js
@@ -1,5 +1,13 @@
 callback([
     {
+        "status": "good", 
+        "new_global_status": "good", 
+        "service": "copr", 
+        "changetype": "single", 
+        "message": "Everything seems to be working.", 
+        "changedate": 1435055811.817356
+    }, 
+    {
         "status": "scheduled", 
         "new_global_status": "scheduled", 
         "service": "copr", 
