af890acc020b37f5ce5e49e497a78e5f300f9ca1
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-10-23 01:04:55 +0200

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -34,9 +34,9 @@ callback({
             "url": "https://admin.fedoraproject.org/updates/"
         }, 
         "copr": {
-            "message": "COPR is backlogged due to builds not running for a long time, recovering now", 
+            "message": "Everything seems to be working.", 
             "name": "COPR Build System", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://copr.fedoraproject.org/"
         }, 
         "darkserver": {
