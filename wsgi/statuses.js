a4268966aadd8e22e5375ef2fc1ca02389c5ec15
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-10-31 22:07:28 +0100

    clear-copr

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -34,9 +34,9 @@ callback({
             "url": "https://admin.fedoraproject.org/updates/"
         }, 
         "copr": {
-            "message": "Backlog being processed.", 
+            "message": "Everything seems to be working.",
             "name": "COPR Build System", 
-            "status": "minor", 
+            "status": "good", 
             "url": "https://copr.fedoraproject.org/"
         }, 
         "darkserver": {
