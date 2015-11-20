208788cd8bc6dbefdc26562a4e7749dd6351de08
Ralph Bean <rbean@redhat.com>
2015-11-19 22:58:00 -0500

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -124,9 +124,9 @@ callback({
             "url": "http://mirrors.fedoraproject.org/"
         }, 
         "packages": {
-            "message": "Rebuilding index for an upgrade.", 
+            "message": "Everything seems to be working.", 
             "name": "Fedora Packages App", 
-            "status": "scheduled", 
+            "status": "good", 
             "url": "https://apps.fedoraproject.org/packages"
         }, 
         "people": {
