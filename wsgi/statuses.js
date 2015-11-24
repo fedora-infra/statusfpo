16cd3db801a67af9e7a13165ff48f4fbc15f485a
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-11-24 08:18:04 +0100

    New service status for ['copr']: major, with message: COPR triggers a bug in OpenStack. Being debugged.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -34,9 +34,9 @@ callback({
             "url": "https://admin.fedoraproject.org/updates/"
         }, 
         "copr": {
-            "message": "Everything seems to be working.", 
+            "message": "COPR triggers a bug in OpenStack. Being debugged.", 
             "name": "COPR Build System", 
-            "status": "good", 
+            "status": "major", 
             "url": "https://copr.fedoraproject.org/"
         }, 
         "darkserver": {
