033bc1ce9e6ab4d6646e41068d863b8a4a11e0b8
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-12-10 01:06:04 +0100

    New service status for ['copr']: major, with message: File system corruption caused by storage issues, being worked on.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -34,9 +34,9 @@ callback({
             "url": "https://admin.fedoraproject.org/updates/"
         }, 
         "copr": {
-            "message": "Everything seems to be working.", 
+            "message": "File system corruption caused by storage issues, being worked on.", 
             "name": "COPR Build System", 
-            "status": "good", 
+            "status": "major", 
             "url": "https://copr.fedoraproject.org/"
         }, 
         "darkserver": {
