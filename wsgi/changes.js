c86e09c709378602a7881058a1e876116c0c4d83
Ralph Bean <rbean@redhat.com>
2015-12-15 17:34:12 -0500

    New service status for ['website']: major, with message: Websites being rebuild

--- wsgi/changes.js
+++ wsgi/changes.js
@@ -1,5 +1,13 @@
 callback([
     {
+        "status": "major", 
+        "new_global_status": "major", 
+        "service": "website", 
+        "changetype": "single", 
+        "message": "Websites being rebuild", 
+        "changedate": 1450218852.446691
+    }, 
+    {
         "status": "good", 
         "new_global_status": "good", 
         "service": "copr", 
