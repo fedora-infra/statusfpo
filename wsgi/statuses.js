043d0a38cbe41df49fb558246f0c5ff95025f46e
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-03-30 17:17:45 +0000

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -34,9 +34,9 @@ callback({
             "url": "https://admin.fedoraproject.org/updates/"
         }, 
         "copr": {
-            "message": "cloud outage", 
+            "message": "Everything seems to be working.", 
             "name": "COPR Build System", 
-            "status": "major", 
+            "status": "good", 
             "url": "https://copr.fedoraproject.org/"
         }, 
         "darkserver": {
