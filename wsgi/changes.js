2b3622540b568adeb52e65b2fb3b156d63c6d68f
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-04-21 20:17:07 +0000

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/changes.js
+++ wsgi/changes.js
@@ -1,5 +1,13 @@
 callback([
     {
+        "status": "good", 
+        "new_global_status": "good", 
+        "service": "wiki", 
+        "changetype": "single", 
+        "message": "Everything seems to be working.", 
+        "changedate": 1461269826.983101
+    }, 
+    {
         "status": "major", 
         "new_global_status": "major", 
         "service": "wiki", 
