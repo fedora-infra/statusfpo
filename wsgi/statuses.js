e90451569e9cf2186cc201e9b8abd127c922bf65
Patrick Uiterwijk <puiterwijk@redhat.com>
2017-04-07 01:19:23 +0200

    New service status for ['-']: good, with message: Everything seems to be working.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -100,9 +100,9 @@ callback({
             "url": "http://fedoraproject.org/wiki/FreeMedia"
         },
         "koji": {
-            "message": "Network outage, being worked on",
+            "message": "Everything seems to be working.",
             "name": "The Koji Buildsystem",
-            "status": "major",
+            "status": "good",
             "url": "http://koji.fedoraproject.org/koji/"
         },
         "koschei": {
