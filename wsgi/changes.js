3e898a0a61cd7ad58bd7e5f56b4eb6ab91e640f9
Kevin Fenzi <kevin@scrye.com>
2017-08-30 14:48:44 -0400

    New service status for ['pkgs']: major, with message: src.fedoraproject.org / pkgs down

--- wsgi/changes.js
+++ wsgi/changes.js
@@ -1,6 +1,14 @@
 callback([
     {
         "changetype": "single",
+        "service": "pkgs",
+        "status": "major",
+        "new_global_status": "major",
+        "message": "src.fedoraproject.org / pkgs down",
+        "changedate": 1504118924.845832
+    },
+    {
+        "changetype": "single",
         "service": "wiki",
         "status": "good",
         "new_global_status": "good",
