7f6372199c1a5bc87773eb666a2a76fb2e11df5a
Rick Elrod <rick@elrod.me>
2018-07-20 01:57:29 -0400

    New service status for ['-']: major, with message: Apparent networking outage in our primary colo.

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -4,172 +4,172 @@ callback({
     "global_verbose_status": "There are scheduled downtimes in progress",
     "services": {
         "authentication": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Ipsilon",
-            "status": "good",
+            "status": "major",
             "url": "https://id.fedoraproject.org/"
         },
         "badges": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Badges",
-            "status": "good",
+            "status": "major",
             "url": "http:/badges.fedoraproject.org/"
         },
         "blockerbugs": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Blockerbugs",
-            "status": "good",
+            "status": "major",
             "url": "http://qa.fedoraproject.org/blockerbugs/"
         },
         "bodhi": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Package Updates Manager",
-            "status": "good",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/updates/"
         },
         "cloud": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora Infrastructure Cloud",
-            "status": "good",
+            "status": "major",
             "url": "https://fedorainfracloud.org/"
         },
         "copr": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "COPR Build System",
-            "status": "good",
+            "status": "major",
             "url": "https://copr.fedoraproject.org/"
         },
         "docs": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Documentation website",
-            "status": "good",
+            "status": "major",
             "url": "http://docs.fedoraproject.org/"
         },
         "elections": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora elections",
-            "status": "good",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/voting/"
         },
         "fas": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Account System",
-            "status": "good",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/accounts/"
         },
         "fedmsg": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora Messaging Bus",
-            "status": "good",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/pkgdb/acls/name/fedmsg"
         },
         "fedocal": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora Calendar",
-            "status": "good",
+            "status": "major",
             "url": "https://apps.fedoraproject.org/fedocal/"
         },
         "fedorapaste": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora pastebin service",
-            "status": "good",
+            "status": "major",
             "url": "https://paste.fedorahosted.org/"
         },
         "koji": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "The Koji Buildsystem",
-            "status": "good",
+            "status": "major",
             "url": "http://koji.fedoraproject.org/koji/"
         },
         "koschei": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Koschei Continuous Integration",
-            "status": "good",
+            "status": "major",
             "url": "https://apps.fedoraproject.org/koschei/"
         },
         "krb": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Kerberos",
-            "status": "good",
+            "status": "major",
             "url": "https://id.fedoraproject.org/"
         },
         "mailinglists": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Mailing Lists",
-            "status": "good",
+            "status": "major",
             "url": "http://lists.fedoraproject.org/"
         },
         "mirrorlist": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Mirror List",
-            "status": "good",
+            "status": "major",
             "url": "http://mirrors.fedoraproject.org/publiclist/"
         },
         "mirrormanager": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Mirror Manager",
-            "status": "good",
+            "status": "major",
             "url": "http://mirrors.fedoraproject.org/"
         },
         "packages": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora Packages App",
-            "status": "good",
+            "status": "major",
             "url": "https://apps.fedoraproject.org/packages"
         },
         "pagure": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Pagure",
-            "status": "good",
+            "status": "major",
             "url": "https://pagure.io"
         },
         "people": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora People",
-            "status": "good",
+            "status": "major",
             "url": "https://fedorapeople.org/"
         },
         "pkgdb": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Package Database",
-            "status": "good",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/pkgdb/"
         },
         "pkgs": {
             "mailinglists": "Fedora mailing lists",
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Package maintainers git repositories",
-            "status": "good",
+            "status": "major",
             "url": "http://pkgs.fedoraproject.org/"
         },
         "registry": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora Container Registry",
-            "status": "good",
+            "status": "major",
             "url": "https://registry.fedoraproject.org/"
         },
         "tagger": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Tagger",
-            "status": "good",
+            "status": "major",
             "url": "https://apps.fedoraproject.org/tagger/"
         },
         "website": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora websites",
-            "status": "good",
+            "status": "major",
             "url": "http://fedoraproject.org/"
         },
         "wiki": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Fedora Wiki",
-            "status": "good",
+            "status": "major",
             "url": "https://fedoraproject.org/wiki/Fedora_Project_Wiki"
         },
         "zodbot": {
-            "message": "Everything seems to be working.",
+            "message": "Apparent networking outage in our primary colo.",
             "name": "Zodbot IRC bot",
-            "status": "good",
+            "status": "major",
             "url": "https://fedoraproject.org/wiki/Zodbot"
         }
     }
