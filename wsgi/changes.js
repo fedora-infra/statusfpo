53699ee9c3f5e7558c482db7c8e50699ae6c4b10
Pavel Raiskup <praiskup@redhat.com>
2020-02-18 16:22:31 +0100

    New service status for ['copr']: good, with message: Everything seems to be working.

--- wsgi/changes.js
+++ wsgi/changes.js
@@ -2,6 +2,14 @@ callback([
     {
         "changetype": "single",
         "service": "copr",
+        "status": "good",
+        "new_global_status": "good",
+        "message": "Everything seems to be working.",
+        "changedate": 1582039351.628882
+    },
+    {
+        "changetype": "single",
+        "service": "copr",
         "status": "scheduled",
         "new_global_status": "scheduled",
         "message": "testing status",
