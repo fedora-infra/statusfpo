c4b24d65482be12c724f4afd254493d61586adae
Stephen Smoogen <smooge@gmail.com>
2020-06-19 08:55:09 -0400

    New service status for ['-']: major, with message: The IAD2 datacentre has a major network issue

--- wsgi/statuses.js
+++ wsgi/statuses.js
@@ -4,172 +4,172 @@ callback({
     "global_verbose_status": "There are scheduled downtimes in progress",
     "services": {
         "authentication": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Ipsilon",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://id.fedoraproject.org/"
         },
         "badges": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Badges",
-            "status": "scheduled",
+            "status": "major",
             "url": "http:/badges.fedoraproject.org/"
         },
         "blockerbugs": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Blockerbugs",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://qa.fedoraproject.org/blockerbugs/"
         },
         "bodhi": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Package Updates Manager",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/updates/"
         },
         "cloud": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora Infrastructure Cloud",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://fedorainfracloud.org/"
         },
         "copr": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "COPR Build System",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://copr.fedoraproject.org/"
         },
         "docs": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Documentation website",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://docs.fedoraproject.org/"
         },
         "elections": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora elections",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/voting/"
         },
         "fas": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Account System",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/accounts/"
         },
         "fedmsg": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora Messaging Bus",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://admin.fedoraproject.org/pkgdb/acls/name/fedmsg"
         },
         "fedocal": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora Calendar",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://apps.fedoraproject.org/fedocal/"
         },
         "fedorapaste": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora pastebin service",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://paste.fedorahosted.org/"
         },
         "koji": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "The Koji Buildsystem",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://koji.fedoraproject.org/koji/"
         },
         "koschei": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Koschei Continuous Integration",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://apps.fedoraproject.org/koschei/"
         },
         "krb": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Kerberos",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://id.fedoraproject.org/"
         },
         "mailinglists": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Mailing Lists",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://lists.fedoraproject.org/"
         },
         "mbs": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Module Build Service",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://mbs.fedoraproject.org"
         },
         "mirrorlist": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Mirror List",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://mirrors.fedoraproject.org/publiclist/"
         },
         "mirrormanager": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Mirror Manager",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://mirrors.fedoraproject.org/"
         },
         "packages": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora Packages App",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://apps.fedoraproject.org/packages"
         },
         "pagure": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Pagure",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://pagure.io"
         },
         "people": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora People",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://fedorapeople.org/"
         },
         "pkgs": {
             "mailinglists": "Fedora mailing lists",
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Package maintainers git repositories",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://pkgs.fedoraproject.org/"
         },
         "registry": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora Container Registry",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://registry.fedoraproject.org/"
         },
         "retrace": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "ABRT Server",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://retrace.fedoraproject.org/"
         },
         "website": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora websites",
-            "status": "scheduled",
+            "status": "major",
             "url": "http://fedoraproject.org/"
         },
         "wiki": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Fedora Wiki",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://fedoraproject.org/wiki/Fedora_Project_Wiki"
         },
         "zodbot": {
-            "message": "We are running from new datacentre with limited capacity",
+            "message": "The IAD2 datacentre has a major network issue",
             "name": "Zodbot IRC bot",
-            "status": "scheduled",
+            "status": "major",
             "url": "https://fedoraproject.org/wiki/Zodbot"
         }
     }
